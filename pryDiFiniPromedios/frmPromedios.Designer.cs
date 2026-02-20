da63200fba3c60d6994d14e8cf3fa63ebaf0ac13
Zaira Di Fini <zairadifini123@gmail.com>
2026-02-20 15:27:39 -0300

    hola

--- pryDiFiniPromedios/frmPromedios.Designer.cs
+++ pryDiFiniPromedios/frmPromedios.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace pryDiFiniPromedios
 {
-    partial class Form1
+    partial class frmPromedios
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            SuspendLayout();
+            // 
+            // frmPromedios
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Name = "frmPromedios";
+            Text = "frmPromedio";
+            ResumeLayout(false);
         }
 
         #endregion
--- pryDiFiniPromedios/frmPromedios.cs
+++ pryDiFiniPromedios/frmPromedios.cs
@@ -1,8 +1,8 @@
 namespace pryDiFiniPromedios
 {
-    public partial class Form1 : Form
+    public partial class frmPromedios : Form
     {
-        public Form1()
+        public frmPromedios()
         {
             InitializeComponent();
         }
